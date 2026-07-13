c61c099f4f0d4342a3ac9915e86aa0bd7d705830
jjh2kr-stack <jjh2kr@gmail.com>
2026-07-13 13:24:50 +0900

    Delete api directory

--- api/health.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    searchClientId: Boolean(process.env.NAVER_SEARCH_CLIENT_ID),
-    searchClientSecret: Boolean(process.env.NAVER_SEARCH_CLIENT_SECRET)
-  });
-}
